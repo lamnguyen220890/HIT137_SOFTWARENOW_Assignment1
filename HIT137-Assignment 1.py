ebc0952bae69ea1cb6d062ade8ef4b9a8475d799
lamnguyen220890 <163125902+lamnguyen220890@users.noreply.github.com>
2024-03-25 21:27:31 +0930

    Add files via upload

---
+++ HIT137-Assignment 1.py
@@ -0,0 +1,114 @@
+###############################################################################################
+                                #HIT137 SOFTWARE NOW – Assignment 1#
+
+# Lecturer name: Abhijith Beeravolu
+# Group members: 
+#        - Son Lam Nguyen (Student ID: s377342)
+#        - Chirag Dudhat (Student ID: s374835) 
+#        - Duncan Nganga (Student ID: s357281)
+#        - Sergei Mar Colo Santos (Student ID: s371673)
+
+# Topic: Video Processing Models
+#        - e. 3D CNNs
+#        - f. RNNs with CNNs
+# GitHub group link: https://github.com/lamnguyen220890/HIT137_SOFTWARENOW_Assignment1.git
+
+###############################################################################################
+
+# Task 1:
+
+# a. Introduction  to the domain and its significance
+#    In recent years, the field of Artificial Intelligence (AI) has achieved outstanding growth, bringing 
+#    about significant changes in many industrial sectors by adopting its applications. One of the important 
+#    branches of AI is Video Processing, where algorithms are developed to understand and analyze data from videos. 
+#    Video modeling is an important field in artificial intelligence (AI), providing the ability to analyze and 
+#    understand video data like humans. In today's digital age, video has become an important source of information, 
+#    from security surveillance to entertainment and education. In this context, video processing models, 
+#    such as 3D Convolutional Neural Networks (3D CNNs) and Recurrent Neural Networks (RNNs) combined 
+#    with Convolutional Neural Networks (CNNs), play an important role in information extraction, object recognition, 
+#    and behavior prediction from video data.
+
+# b. Key applications
+#    Video processing models play an important role in many different real-world contexts. 
+#    - Security surveillance: Video processing models are widely used in security surveillance systems to detect and
+#    identify suspicious behavior. 3D CNN can analyze video data to detect objects, recognize faces, and track objects
+#    in real-time. RNN combined with CNN can detect unusual behavioral patterns and alert the system when notable events occur. 
+
+#    - Entertainment and movies: In the entertainment industry, video processing models are used to create special effects, enhance
+#    image quality, and automate the film production process. 3D CNN can learn and reproduce motion segments, while RNN can analyze and 
+#    create similar scenarios with data learned from previous movies. Education and training: 
+
+#    - In the field of education, video processing models can be used to detect and evaluate student learning behavior, provide
+#    instant feedback, and personalize the learning process. 3D CNN and RNN can analyze gestures, expressions, and interactions
+#    in online classrooms or distance learning systems.
+
+#    - In Healthcare, these models are used to analyze medical images, allowing early detection of diseases such as cancer. 
+#    For example, researchers have used 3D CNN to analyze MRI scans to segment brain tumors, facilitating treatment planning
+#    and accurate diagnosis.
+
+# c. Impact on Society and Ethical Considerations
+#    Although the video processing model brings many benefits to society, it also raises many ethical and privacy issues. 
+#    The rise of surveillance technology can pose a threat to privacy and personal freedom. Therefore, careful controls 
+#    and management measures are needed to ensure that the use of the video processing model is reasonable and ethical.
+
+# d. Tools and libraries are available to develop these models.
+#    - The Keras library in Python: Keras is a high-level, deep-learning API developed by Google for implementing neural networks. 
+#    It is written in Python and is used to make the implementation of neural networks easy. It also supports multiple backend 
+#    neural network computations.
+
+#    - PyTorch: PyTorch is an open-source machine learning library developed by Facebook's AI Research Lab (FAIR). Provides a 
+#    flexible approach to model building and training, especially in working with 3D data and RNN models.
+
+#    - TensorFlow: is an open-source library developed by Google to build and train machine learning and artificial intelligence 
+#    models. It provides powerful tools and APIs for building 3D CNN and RNN models.
+
+# Artificial intelligence has greatly improved in the healthcare industry, especially in machine learning, where algorithms majorly
+# analyze complicated medical data to aid in diagnostic, prognostic, and treatment planning. Convolutional Neural networks are
+# applied in machine learning. 3D CNNs are adopted to process volumetric data, capturing spatial and depth-related information 
+# for thorough analysis.
+# https://doi.org/10.3390/engproc2023059003
+
+# 3D CNN can use nonlinear 3D content information sources like digital imaging and communication in medicine to help detect
+# abnormalities in lungs and lung cancer. CNN is fast and easy to scale, making it easy to classify images, diagnosis, and 
+# semantic division. The article reports the utilization of 3D CNN to detect lung cancer automatically.
+# https://doi.org/10.1155/2021/6695518
+
+# The proposed hybrid 3D-CNN & ConvLSTM model for video-based facial expression recognition demonstrates competitive accuracy 
+# without pre-training on external datasets, making it efficient and accessible for real-time applications. Ethically, its deployment
+#  should consider privacy concerns regarding facial data usage and potential biases in emotion recognition algorithms.
+# Singh, R., Saurav, S., Kumar, T. et al. Facial expression recognition in videos using hybrid CNN & ConvLSTM. Int. j. inf. 
+# tecnol. 15, 1819–1830 (2023). 
+# https://doi.org/10.1007/s41870-023-01183-0 
+
+# Engineers have been able to design machines/robots using Artificial intelligence thanks to the emergence of cloud computing,
+# artificial neural networks, big data, and machine learning technologies. All this helps improve human performance while 
+# interacting with artificial intelligence. Educators, on the other hand, when using Artificial intelligence, lessen the 
+# burden on the students and affect the learning experience. Online learning by Charles Darwin University is a good example of 
+# the use of Artificial intelligence and digitization of learning resources.
+# https://doi.org/10.1155/2021/8812542
+
+# References:
+
+    # Tran, D., Wang, H., Torresani, L., Ray, J., LeCun, Y., & Paluri, M. (2018, June 1). 
+    # A Closer Look at Spatiotemporal Convolutions for Action Recognition. IEEE Xplore. 
+    # https://doi.org/10.1109/CVPR.2018.00675
+
+    # Kailash Ahirwar. (2019). Generative adversarial networks projects. Packt Publishing Ltd.
+    # Koohzadi, M., & Charkari, N. M. (2017). Survey on deep learning methods in human action recognition. 
+    # IET Computer Vision, 11(8), 623–632. https://doi.org/10.1049/iet-cvi.2016.0355
+
+    # Donahue, J., Lisa Anne Hendricks, Guadarrama, S., Rohrbach, M., Subhashini Venugopalan, Darrell, T., & Saenko, K. (2015).
+    # Long-term recurrent convolutional networks for visual recognition and description. Computer Vision and Pattern Recognition. 
+    # https://doi.org/10.1109/cvpr.2015.7298878
+
+    # Ravi, N., Reizenstein, J., Novotny, D. R., Gordon, T., Lo, W.-Y., Johnson, J. C., & Gkioxari, G. (2020).
+    # Accelerating 3D Deep Learning with PyTorch3D. Accelerating 3D Deep Learning with PyTorch3D.
+    # https://doi.org/10.48550/arxiv.2007.08501
+
+    # Kumar, A., Singh, K. P., Kumar, S., & L. Vetrivendan. (2022). Image classification in python using keras. 
+    # Lecture Notes on Data Engineering and Communications Technologies, 90, 541–556. 
+    # https://doi.org/10.1007/978-981-16-6289-8_45
+
+
+# Task 2: 
+#    GitHub repository link: https://github.com/lamnguyen220890/HIT137_SOFTWARENOW_Assignment1
